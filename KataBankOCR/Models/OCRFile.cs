4da428c3593a29970316bffd327a302fa8b23d24
Jacob Hitze <jacob.hitze@eii-online.com>
2016-12-27 22:51:59 -0500

    Start of OCRFile

---
+++ KataBankOCR/Models/OCRFile.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace KataBankOCR.Models
+{
+    public class OCRFile
+    {
+        public OCRFile(Stream fileContents)
+        {
+            FileContents = fileContents;
+        }
+
+        public List<string> Parse()
+        {
+            List<string> accountNumbers = new List<string>();
+            // Read through stream
+            // find line breaks
+            // build groupings of account numbers
+            // use OCRAccountNumberParser class to parse groupings
+            // return List<string>AccountNumbers
+            return accountNumbers;
+        }
+
+        private Stream FileContents
+        {
+            get;
+            set;
+        }
+    }
+}

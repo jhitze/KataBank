0155638eeba7864abd05505c2697aa21a21d1d46
Jacob Hitze <jacob.hitze@eii-online.com>
2016-12-28 00:14:19 -0500

    Added tests for all same digits

--- KataBankOCR.Tests/Models/OCRAccountNumberParserTests.cs
+++ KataBankOCR.Tests/Models/OCRAccountNumberParserTests.cs
@@ -40,5 +40,125 @@ namespace KataBankOCR.Models.Tests
 
             Assert.AreEqual("111111111", result);
         }
+
+        [TestMethod()]
+        public void ParseLinesTest_User1_AllTwos()
+        {
+            List<string> useCase1 = new List<string>();
+            useCase1.Add(" _  _  _  _  _  _  _  _  _ ");
+            useCase1.Add(" _| _| _| _| _| _| _| _| _|");
+            useCase1.Add("|_ |_ |_ |_ |_ |_ |_ |_ |_ ");
+            useCase1.Add("                           ");
+
+            OCRAccountNumberParser parser = new OCRAccountNumberParser();
+            string result = parser.ParseLines(useCase1);
+
+            Assert.AreEqual("222222222", result);
+        }
+
+        [TestMethod()]
+        public void ParseLinesTest_User1_AllThrees()
+        {
+            List<string> useCase1 = new List<string>();
+            useCase1.Add(" _  _  _  _  _  _  _  _  _ ");
+            useCase1.Add(" _| _| _| _| _| _| _| _| _|");
+            useCase1.Add(" _| _| _| _| _| _| _| _| _|");
+            useCase1.Add("                           ");
+
+            OCRAccountNumberParser parser = new OCRAccountNumberParser();
+            string result = parser.ParseLines(useCase1);
+
+            Assert.AreEqual("333333333", result);
+        }
+
+        [TestMethod()]
+        public void ParseLinesTest_User1_AllFours()
+        {
+            List<string> useCase1 = new List<string>();
+            useCase1.Add("                           ");
+            useCase1.Add("|_||_||_||_||_||_||_||_||_|");
+            useCase1.Add("  |  |  |  |  |  |  |  |  |");
+            useCase1.Add("                           ");
+
+            OCRAccountNumberParser parser = new OCRAccountNumberParser();
+            string result = parser.ParseLines(useCase1);
+
+            Assert.AreEqual("444444444", result);
+        }
+
+        [TestMethod()]
+        public void ParseLinesTest_User1_AllFives()
+        {
+            List<string> useCase1 = new List<string>();
+            useCase1.Add(" _  _  _  _  _  _  _  _  _ ");
+            useCase1.Add("|_ |_ |_ |_ |_ |_ |_ |_ |_ ");
+            useCase1.Add(" _| _| _| _| _| _| _| _| _|");
+            useCase1.Add("                           ");
+
+            OCRAccountNumberParser parser = new OCRAccountNumberParser();
+            string result = parser.ParseLines(useCase1);
+
+            Assert.AreEqual("555555555", result);
+        }
+
+        [TestMethod()]
+        public void ParseLinesTest_User1_AllSixes()
+        {
+            List<string> useCase1 = new List<string>();
+            useCase1.Add(" _  _  _  _  _  _  _  _  _ ");
+            useCase1.Add("|_ |_ |_ |_ |_ |_ |_ |_ |_ ");
+            useCase1.Add("|_||_||_||_||_||_||_||_||_|");
+            useCase1.Add("                           ");
+
+            OCRAccountNumberParser parser = new OCRAccountNumberParser();
+            string result = parser.ParseLines(useCase1);
+
+            Assert.AreEqual("666666666", result);
+        }
+
+        [TestMethod()]
+        public void ParseLinesTest_User1_AllSevens()
+        {
+            List<string> useCase1 = new List<string>();
+            useCase1.Add(" _  _  _  _  _  _  _  _  _ ");
+            useCase1.Add("  |  |  |  |  |  |  |  |  |");
+            useCase1.Add("  |  |  |  |  |  |  |  |  |");
+            useCase1.Add("                           ");
+
+            OCRAccountNumberParser parser = new OCRAccountNumberParser();
+            string result = parser.ParseLines(useCase1);
+
+            Assert.AreEqual("777777777", result);
+        }
+
+        [TestMethod()]
+        public void ParseLinesTest_User1_AllEights()
+        {
+            List<string> useCase1 = new List<string>();
+            useCase1.Add(" _  _  _  _  _  _  _  _  _ ");
+            useCase1.Add("|_||_||_||_||_||_||_||_||_|");
+            useCase1.Add("|_||_||_||_||_||_||_||_||_|");
+            useCase1.Add("                           ");
+
+            OCRAccountNumberParser parser = new OCRAccountNumberParser();
+            string result = parser.ParseLines(useCase1);
+
+            Assert.AreEqual("888888888", result);
+        }
+
+        [TestMethod()]
+        public void ParseLinesTest_User1_AllNines()
+        {
+            List<string> useCase1 = new List<string>();
+            useCase1.Add(" _  _  _  _  _  _  _  _  _ ");
+            useCase1.Add("|_||_||_||_||_||_||_||_||_|");
+            useCase1.Add(" _| _| _| _| _| _| _| _| _|");
+            useCase1.Add("                           ");
+
+            OCRAccountNumberParser parser = new OCRAccountNumberParser();
+            string result = parser.ParseLines(useCase1);
+
+            Assert.AreEqual("999999999", result);
+        }
     }
 }

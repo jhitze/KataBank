08c74fb4c8e967c7a8a2afaec81cdcbe28c308ee
Jacob Hitze <jacob.hitze@eii-online.com>
2016-12-27 23:49:45 -0500

    Got the rest of the individual numbers working and tested.

--- KataBankOCR.Tests/Models/OCRNumberParserTests.cs
+++ KataBankOCR.Tests/Models/OCRNumberParserTests.cs
@@ -30,5 +30,87 @@ namespace KataBankOCR.Models.Tests
 
             Assert.AreEqual("1", result);
         }
+
+        [TestMethod()]
+        public void parseTest_Two()
+        {
+            string[] test = new string[] { " _ ", " _|", "|_ ", "   " };
+
+            string result = OCRNumberParser.parse(test);
+
+            Assert.AreEqual("2", result);
+        }
+
+        [TestMethod()]
+        public void parseTest_Three()
+        {
+            string[] test = new string[] { " _ ", " _|", " _|", "   " };
+
+            string result = OCRNumberParser.parse(test);
+
+            Assert.AreEqual("3", result);
+        }
+
+        [TestMethod()]
+        public void parseTest_Four()
+        {
+            string[] test = new string[] { "   ", "|_|", "  |", "   " };
+
+            string result = OCRNumberParser.parse(test);
+
+            Assert.AreEqual("4", result);
+        }
+
+        [TestMethod()]
+        public void parseTest_Five()
+        {
+            string[] test = new string[] { " _ ", "|_ ", " _|", "   " };
+
+            string result = OCRNumberParser.parse(test);
+
+            Assert.AreEqual("5", result);
+        }
+
+        [TestMethod()]
+        public void parseTest_Six()
+        {
+            string[] test = new string[] { " _ ", "|_ ", "|_|", "   " };
+
+            string result = OCRNumberParser.parse(test);
+
+            Assert.AreEqual("6", result);
+        }
+
+        [TestMethod()]
+        public void parseTest_Seven()
+        {
+            string[] test = new string[] { " _ ", "  |", "  |", "   " };
+
+            string result = OCRNumberParser.parse(test);
+
+            Assert.AreEqual("7", result);
+        }
+
+        [TestMethod()]
+        public void parseTest_Eight()
+        {
+            string[] test = new string[] { " _ ", "|_|", "|_|", "   " };
+
+            string result = OCRNumberParser.parse(test);
+
+            Assert.AreEqual("8", result);
+        }
+
+        [TestMethod()]
+        public void parseTest_Nine()
+        {
+            string[] test = new string[] { " _ ", "|_|", " _|", "   " };
+
+            string result = OCRNumberParser.parse(test);
+
+            Assert.AreEqual("9", result);
+        }
+
+
     }
 }
--- KataBankOCR/Models/OCRNumberParser.cs
+++ KataBankOCR/Models/OCRNumberParser.cs
@@ -7,8 +7,54 @@ namespace KataBankOCR.Models
 {
     public static class OCRNumberParser
     {
-        private static string[] zero = new string[] { " _ ", "| |", "|_|", "   " };
-        private static string[] one = new string[] { "   ", "  |", "  |", "   " };
+        private static string[] zero = new string[] { " _ ",
+                                                      "| |",
+                                                      "|_|",
+                                                      "   " };
+        private static string[] one = new string[] { "   ",
+                                                     "  |",
+                                                     "  |",
+                                                     "   " };
+
+        private static string[] two = new string[] { " _ ",
+                                                     " _|",
+                                                     "|_ ",
+                                                     "   " };
+
+        private static string[] three = new string[] { " _ ",
+                                                       " _|",
+                                                       " _|",
+                                                       "   " };
+
+        private static string[] four = new string[] { "   ",
+                                                      "|_|",
+                                                      "  |",
+                                                      "   " };
+
+        private static string[] five = new string[] { " _ ",
+                                                      "|_ ",
+                                                      " _|",
+                                                      "   " };
+
+        private static string[] six = new string[] { " _ ",
+                                                     "|_ ",
+                                                     "|_|",
+                                                     "   " };
+
+        private static string[] seven = new string[] { " _ ",
+                                                       "  |",
+                                                       "  |",
+                                                       "   " };
+
+        private static string[] eight = new string[] { " _ ",
+                                                       "|_|",
+                                                       "|_|",
+                                                       "   " };
+
+        private static string[] nine = new string[] { " _ ",
+                                                      "|_|",
+                                                      " _|",
+                                                      "   " };
 
 
         public static string parse(string[] suspectedNumber)
@@ -21,6 +67,38 @@ namespace KataBankOCR.Models
             else if (suspectedNumber.OrderBy(a => a).SequenceEqual(one.OrderBy(a => a)))
             {
                 return "1";
+            }
+            else if (suspectedNumber.OrderBy(a => a).SequenceEqual(two.OrderBy(a => a)))
+            {
+                return "2";
+            }
+            else if (suspectedNumber.OrderBy(a => a).SequenceEqual(three.OrderBy(a => a)))
+            {
+                return "3";
+            }
+            else if (suspectedNumber.OrderBy(a => a).SequenceEqual(four.OrderBy(a => a)))
+            {
+                return "4";
+            }
+            else if (suspectedNumber.OrderBy(a => a).SequenceEqual(five.OrderBy(a => a)))
+            {
+                return "5";
+            }
+            else if (suspectedNumber.OrderBy(a => a).SequenceEqual(six.OrderBy(a => a)))
+            {
+                return "6";
+            }
+            else if (suspectedNumber.OrderBy(a => a).SequenceEqual(seven.OrderBy(a => a)))
+            {
+                return "7";
+            }
+            else if (suspectedNumber.OrderBy(a => a).SequenceEqual(eight.OrderBy(a => a)))
+            {
+                return "8";
+            }
+            else if (suspectedNumber.OrderBy(a => a).SequenceEqual(nine.OrderBy(a => a)))
+            {
+                return "9";
             }
             return "";
         }
